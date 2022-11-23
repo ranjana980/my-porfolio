729a09c90ac4309bab622360637e7433b4216c0b
Ranjana Chaudhary <ranjana20@navgurukul.org>
2022-11-23 23:20:01 +0530

    Changes

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -19,7 +19,7 @@ export default function MyPortFolio() {
                                 <div className='line'></div>
                             </div>
 
-                            <div style={{  textAlign: 'start' }}>
+                            <div style={{ textAlign: 'start' }}>
                                 <div className='d-flex'><h4 style={{ color: 'rgb(0, 123, 255)', textShadow: '1px 1px 1px #000000' }}>Board of HighSchool and Intermediat Educational</h4></div>
                                 <div style={{ color: 'white' }}>
                                     I have Completed My HighSchool From
@@ -36,7 +36,7 @@ export default function MyPortFolio() {
                                 <CastForEducationIcon style={{ fontSize: '30px', marginRight: '10px', marginTop: '7px' }} />
                                 <div className='line'></div>
                             </div>
-                            <div style={{  textAlign: 'start' }}>
+                            <div style={{ textAlign: 'start' }}>
                                 <div className='d-flex'><h4 style={{ color: 'rgb(0, 123, 255)', textShadow: '1px 1px 1px #000000' }}>Board of HighSchool and Intermediat Educational</h4></div>
                                 <div style={{ color: 'white' }}>
                                     I have Completed My Intermediat From
@@ -49,7 +49,8 @@ export default function MyPortFolio() {
                         <h5 className='Year1'>2015 - 2017</h5>
                         <div className='d-flex'>
                             <CastForEducationIcon style={{ fontSize: '30px', marginRight: '10px', marginTop: '7px' }} />
-                            <div style={{  textAlign: 'start' }}>
+                            <div className='line'></div>
+                            <div style={{ textAlign: 'start' }}>
                                 <div className='d-flex'><h4 style={{ color: 'rgb(0, 123, 255)', textShadow: '1px 1px 1px #000000' }}>Deen Dayal Upadhyay University of Gorkhpur</h4></div>
                                 <div style={{ color: 'white' }}>
                                     I have Completed My Bachelor's Degree
@@ -65,13 +66,13 @@ export default function MyPortFolio() {
                                 <CastForEducationIcon style={{ fontSize: '30px', marginRight: '10px', marginTop: '7px' }} />
                                 <div className='line'></div>
                             </div>
-                            <div style={{  textAlign: 'start' }}>
+                            <div style={{ textAlign: 'start' }}>
                                 <div className='d-flex'><h4 style={{ color: 'rgb(0, 123, 255)', textShadow: '1px 1px 1px #000000' }}>Navgurukul</h4></div>
                                 <div style={{ color: 'white' }}>
-                                    I have Completed 1 Year Residential 
+                                    I have Completed 1 Year Residential
                                     Software Engineering program From Navgurukul.
-                                    Navgurukul is a non-profit orgnization which 
-                                    provides scholership Students for thier study for taking 
+                                    Navgurukul is a non-profit orgnization which
+                                    provides scholership Students for thier study for taking
                                     job in Good IT Companies.I learnt Python,html,css,
                                     JavaScript and Reactjs in Navgurukul Pune Campus
                                 </div>
@@ -85,7 +86,7 @@ export default function MyPortFolio() {
                                 <WorkIcon style={{ fontSize: '30px', marginRight: '10px', marginTop: '7px' }} />
                                 <div className='line'></div>
                             </div>
-                            <div style={{  textAlign: 'start' }}>
+                            <div style={{ textAlign: 'start' }}>
                                 <div className='d-flex'><h4 style={{ color: 'rgb(0, 123, 255)', textShadow: '1px 1px 1px #000000' }}>XpressCure Technology</h4></div>
                                 <div style={{ color: 'white' }}>
                                     I have experience in building
@@ -93,14 +94,14 @@ export default function MyPortFolio() {
                                     using the latest technologies.
                                     I have worked on projects using
                                     HTML, CSS, JavaScript, React,
-                                    Redux, Next.js,Bootstrap,Material 
+                                    Redux, Next.js,Bootstrap,Material
                                     Ui,socket.io.I Created A Doctor Web App
                                     from Starting and added login,signup,
                                     Registration,Chat,Video Call
                                     and Prescription,Doctor
                                     can create his Account can
                                     Registration and login his Account For his Data.
-                                    and Doctor can chat Video call and send Prescription 
+                                    and Doctor can chat Video call and send Prescription
                                     for Patient using Mobile phone and web applications.
                                     this web Aplication is Mobile Responsive also.
                                 </div>
@@ -117,7 +118,7 @@ export default function MyPortFolio() {
                         <div className='col-md-4  col-sm-5 col-xs-3'>
                             <div className='d-flex' style={{ justifyContent: 'space-between', color: 'white' }}><h6>JavaScript</h6><h6>70%</h6></div>
                             <LinearProgress variant="determinate" color="secondary" value={70} />
-                        </div> 
+                        </div>
                         <div className='col-md-4  col-sm-5 col-xs-3' >
                             <div className='d-flex' style={{ justifyContent: 'space-between', color: 'white' }}><h6>Html && Css</h6><h6>70%</h6></div>
                             <LinearProgress variant="determinate" color="secondary" value={70} />
@@ -191,7 +192,7 @@ export default function MyPortFolio() {
                     <div className='row mt-4 ml-sm-1 '>
                         <div className='col-md-3 col-sm-2  br bg-white ml-4 mr-sm-5 p-2 col-xs-2'>
                             <div className='hover-div'>
-                                <img className='br  live-preview' src={emmoji}  />
+                                <img className='br  live-preview' src={emmoji} />
                                 <div class="overlay">
                                 </div>
                                 <div class="text"><a target='_blank' href='https://emoji-search-beta.vercel.app/'>Live Preview</a></div>
@@ -209,7 +210,7 @@ export default function MyPortFolio() {
                         </div>
                         <div className='col-md-3 col-sm-2  br bg-white ml-4 mr-sm-5 p-2 col-xs-2'>
                             <div className='hover-div'>
-                                <img className='br live-preview' src={doctor}  />
+                                <img className='br live-preview' src={doctor} />
                                 <div class="overlay">
                                 </div>
                                 <div class="text"><a target='_blank' href='doctor.xpresscure.com'>Live Preview</a></div>
