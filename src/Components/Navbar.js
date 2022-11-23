9532472be586b5143a61e6ec4192cf1754e340ec
Ranjana Chaudhary <ranjana20@navgurukul.org>
2022-11-23 22:02:41 +0530

    toggler changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import CloseIcon from '@material-ui/icons/Close';
+
 export default function Navbar(props) {
+    const [display,setDisplay]=useState("none")
     const handleClick = (type) => {
         console.log('clicked')
         if (type == 'Home') {
@@ -21,10 +25,13 @@ export default function Navbar(props) {
         <nav className="navbar navbar-expand-lg navbar-light bg-skyblue">
             <div className="d-flex container">
                 <div className="Name_title d-flex justify-content-end"><a className="navbar-brand " href="#">Ranjana Chaudhary .</a></div>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                {display=="none"? <button onClick={()=>
+                    setDisplay("block")} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                </button>:<CloseIcon style={{color:'white',fontSize:'40px',position:'relaative',right:'20px'}} onClick={()=>
+                    setDisplay("none")}/>}
+               
+                <div style={{display:display}} className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <a className="nav-link" onClick={() => handleClick('Home')}>Home </a>
@@ -35,10 +42,6 @@ export default function Navbar(props) {
                         <li className="nav-item">
                             <a className="nav-link" onClick={() => handleClick('Portfolio')}>Resume</a>
                         </li>
-
-                        {/* <li className="nav-item ">
-                            <a className="nav-link" onClick={() => handleClick('Blog')}>Blog</a>
-                        </li> */}
                         <li className="nav-item ">
                             <a className="nav-link" onClick={() => handleClick('Contact')}>Contact</a>
                         </li>
